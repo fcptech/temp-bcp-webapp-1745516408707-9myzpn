a326399a0637c3b25e5ea1085e1dfb4ab628f59e
FCTechnology <tech@bcpglobal.net>
2025-04-24 14:42:54 -0300

    Copy src/i18n/locales/components/nav.ts from bcp-webapp

---
+++ src/i18n/locales/components/nav.ts
@@ -0,0 +1,23 @@
+export const nav = {
+  en: {
+    home: "HOME",
+    investments: "INVESTMENTS",
+    insurance: "INSURANCE",
+    faq: "FAQ",
+    legal: "LEGAL"
+  },
+  es: {
+    home: "INICIO",
+    investments: "INVERSIONES",
+    insurance: "SEGUROS",
+    faq: "FAQ",
+    legal: "LEGALES"
+  },
+  pt: {
+    home: "INÍCIO",
+    investments: "INVESTIMENTOS",
+    insurance: "SEGUROS",
+    faq: "FAQ",
+    legal: "LEGAL"
+  }
+};
