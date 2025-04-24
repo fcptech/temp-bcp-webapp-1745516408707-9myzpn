893d75d6f85b59898cce99670d50e468407b455b
FCTechnology <tech@bcpglobal.net>
2025-04-24 14:43:03 -0300

    Copy src/i18n/locales/pages/account.ts from bcp-webapp

---
+++ src/i18n/locales/pages/account.ts
@@ -0,0 +1,59 @@
+export const account = {
+  en: {
+    accountValue: "ACCOUNT VALUE",
+    portfolio: "YOUR PORTFOLIO",
+    assetAllocation: "ASSET ALLOCATION",
+    performance: {
+      netGains: "Net Gains",
+      totalReturn: "Total Return",
+      lastUpdate: "Last Update",
+      monthlyReturn: "MONTHLY RETURN"
+    },
+    tabs: {
+      summary: "SUMMARY",
+      performance: "PERFORMANCE",
+      activity: "ACTIVITY",
+      statements: "STATEMENTS",
+      legal: "LEGAL",
+      settings: "SETTINGS"
+    }
+  },
+  es: {
+    accountValue: "VALOR DE LA CUENTA",
+    portfolio: "TU PORTAFOLIO",
+    assetAllocation: "ASIGNACIÓN DE ACTIVOS",
+    performance: {
+      netGains: "Ganancias Netas",
+      totalReturn: "Rendimiento Total",
+      lastUpdate: "Última Actualización",
+      monthlyReturn: "RENDIMIENTO MENSUAL"
+    },
+    tabs: {
+      summary: "RESUMEN",
+      performance: "DESEMPEÑO",
+      activity: "ACTIVIDAD",
+      statements: "ESTADOS DE CUENTA",
+      legal: "LEGALES",
+      settings: "CONFIGURACIÓN"
+    }
+  },
+  pt: {
+    accountValue: "VALOR DA CONTA",
+    portfolio: "SEU PORTFÓLIO",
+    assetAllocation: "ALOCAÇÃO DE ATIVOS",
+    performance: {
+      netGains: "Ganhos Líquidos",
+      totalReturn: "Retorno Total",
+      lastUpdate: "Última Atualização",
+      monthlyReturn: "RETORNO MENSAL"
+    },
+    tabs: {
+      summary: "RESUMO",
+      performance: "DESEMPENHO",
+      activity: "ATIVIDADE",
+      statements: "EXTRATOS",
+      legal: "LEGAL",
+      settings: "CONFIGURAÇÕES"
+    }
+  }
+};
