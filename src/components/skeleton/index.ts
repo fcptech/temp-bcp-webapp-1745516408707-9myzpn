827efb4a19760870d99d6a6dd9c45ce7b4665df8
FCTechnology <tech@bcpglobal.net>
2025-04-24 14:42:29 -0300

    Copy src/components/skeleton/index.ts from bcp-webapp

---
+++ src/components/skeleton/index.ts
@@ -0,0 +1,7 @@
+export * from './Skeleton';
+export * from './DashboardSkeleton';
+export * from './MarketHubSkeleton';
+export * from './PerformanceViewSkeleton';
+export * from './ActivityViewSkeleton';
+export * from './StatementsViewSkeleton';
+export * from './AccountSettingsSkeleton';
