7d4b4e4d9f775642830f3aa5c71bb91fad671b89
FCTechnology <tech@bcpglobal.net>
2025-04-24 14:42:58 -0300

    Copy src/i18n/locales/en.ts from bcp-webapp

---
+++ src/i18n/locales/en.ts
@@ -0,0 +1,13 @@
+import { login } from './pages/login';
+import { dashboard } from './pages/dashboard';
+import { account } from './pages/account';
+import { nav } from './components/nav';
+import { common } from './components/common';
+
+export default {
+  login: login.en,
+  dashboard: dashboard.en,
+  account: account.en,
+  nav: nav.en,
+  common: common.en
+};
