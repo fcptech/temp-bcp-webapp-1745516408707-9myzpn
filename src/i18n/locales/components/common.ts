679e7bd42dcd91208a52de840c03f75857ca72ac
FCTechnology <tech@bcpglobal.net>
2025-04-24 14:42:52 -0300

    Copy src/i18n/locales/components/common.ts from bcp-webapp

---
+++ src/i18n/locales/components/common.ts
@@ -0,0 +1,14 @@
+export const common = {
+  en: {
+    signOut: "Sign Out",
+    saveChanges: "Save Changes"
+  },
+  es: {
+    signOut: "Cerrar sesión",
+    saveChanges: "Guardar Cambios"
+  },
+  pt: {
+    signOut: "Sair",
+    saveChanges: "Salvar Alterações"
+  }
+};
